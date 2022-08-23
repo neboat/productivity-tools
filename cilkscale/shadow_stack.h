ff0ea410b71baaa3d0578bc4031b474ab5aebdf7
TB Schardl <neboat@mit.edu>
2022-08-23 13:15:00 +0000

    [cilkscale] Changes to use a bitcode ABI and a tool-defined stack structure.

--- cilkscale/shadow_stack.h
+++ cilkscale/shadow_stack.h
@@ -52,17 +52,103 @@ struct shadow_stack_frame_t {
   frame_type type = frame_type::NONE;
 
   // Initialize the stack frame. 
-  void init(frame_type _type) {
+  void init(frame_type _type, cilk_time_t p_contin_work = cilk_time_t::zero(),
+            cilk_time_t p_contin_span = cilk_time_t::zero(),
+            cilk_time_t p_contin_bspan = cilk_time_t::zero()) {
     type = _type;
     achild_work = cilk_time_t::zero();
-    contin_work = cilk_time_t::zero();
+    contin_work = p_contin_work;
     lchild_span = cilk_time_t::zero();
-    contin_span = cilk_time_t::zero();
+    contin_span = p_contin_span;
     lchild_bspan = cilk_time_t::zero();
-    contin_bspan = cilk_time_t::zero();
+    contin_bspan = p_contin_bspan;
   }
 };
 
+struct shadow_stack_top_bot_t {
+  // Start and stop timers for measuring the execution time of a strand.
+  cilkscale_timer_t start;
+  cilkscale_timer_t stop;
+
+  // Top and bottom stack frames.
+  shadow_stack_frame_t top;
+  shadow_stack_frame_t *bot;
+
+  shadow_stack_top_bot_t(frame_type type = frame_type::MAIN) {
+    top.init(type);
+    bot = &top;
+  }
+  shadow_stack_top_bot_t(const shadow_stack_top_bot_t &copy) : top(copy.top) {
+    if (copy.bot == &copy.top)
+      bot = &top;
+    else
+      bot = copy.bot;
+  }
+
+  shadow_stack_frame_t &get_top() { return top; }
+  shadow_stack_frame_t &get_bot() const { return *bot; }
+  void set_bot(shadow_stack_frame_t &frame) { bot = &frame; }
+
+  /// Reducer support
+
+  static void identity(void *view) {
+    new (view) shadow_stack_top_bot_t(frame_type::SPAWNER);
+  }
+
+  static void reduce(void *left_view, void *right_view) {
+    shadow_stack_top_bot_t *left =
+        static_cast<shadow_stack_top_bot_t *>(left_view);
+    shadow_stack_top_bot_t *right =
+        static_cast<shadow_stack_top_bot_t *>(right_view);
+
+    shadow_stack_frame_t &l_bot = *left->bot;
+    shadow_stack_frame_t &r_bot = *right->bot;
+
+    assert(frame_type::SPAWNER == r_bot.type);
+    assert(right->bot == &right->top);
+
+#if TRACE_CALLS
+    fprintf(stderr, "left contin_work = %ld\nleft achild_work = %ld\n"
+            "right contin_work = %ld\nright achild_work = %ld\n",
+            l_bot.contin_work, l_bot.achild_work,
+            r_bot.contin_work, r_bot.achild_work);
+    fprintf(stderr, "left contin = %ld\nleft child = %ld\n"
+            "right contin = %ld\nright child = %ld\n",
+            l_bot.contin_span, l_bot.lchild_span,
+            r_bot.contin_span, r_bot.lchild_span);
+    fprintf(stderr, "left contin bspan = %ld\nleft child bspan = %ld\n"
+            "right contin bspan = %ld\nright child bspan = %ld\n",
+            l_bot.contin_bspan, l_bot.lchild_bspan,
+            r_bot.contin_bspan, r_bot.lchild_bspan);
+#endif
+
+    // Add the work variables from the right stack into the left.
+    l_bot.contin_work += r_bot.contin_work;
+    l_bot.achild_work += r_bot.achild_work;
+
+    // If the left stack has a longer path from the root to the end of its
+    // longest child, set this new span in keft.
+    if (l_bot.contin_span + r_bot.lchild_span > l_bot.lchild_span) {
+      l_bot.lchild_span = l_bot.contin_span + r_bot.lchild_span;
+    }
+    // Add the continuation span from the right stack into the left.
+    l_bot.contin_span += r_bot.contin_span;
+
+    // If the left stack has a longer path from the root to the end of its
+    // longest child, set this new span in keft.
+    if (l_bot.contin_bspan + r_bot.lchild_bspan > l_bot.lchild_bspan) {
+      l_bot.lchild_bspan = l_bot.contin_bspan + r_bot.lchild_bspan;
+    }
+    // Add the continuation span from the right stack into the left.
+    l_bot.contin_bspan += r_bot.contin_bspan;
+
+    right->~shadow_stack_top_bot_t();
+  }
+
+  duration_t elapsed_time() {
+    return ::elapsed_time(&stop, &start);
+  }
+};
 
 using stack_index_t = int32_t;
 
@@ -108,7 +194,10 @@ public:
     return frames[bot];
   }
 
-  shadow_stack_frame_t &push(frame_type type) {
+  shadow_stack_frame_t &push(frame_type type,
+                             cilk_time_t p_contin_work = cilk_time_t::zero(),
+                             cilk_time_t p_contin_span = cilk_time_t::zero(),
+                             cilk_time_t p_contin_bspan = cilk_time_t::zero()) {
     ++bot;
 
     // If necessary, double the capacity of the shadow stack.
@@ -129,7 +218,7 @@ public:
       delete[] old_frames;
     }
 
-    frames[bot].init(type);
+    frames[bot].init(type, p_contin_work, p_contin_span, p_contin_bspan);
     return frames[bot];
   }
 
@@ -202,5 +291,9 @@ public:
 typedef shadow_stack_t _Hyperobject(shadow_stack_t::identity,
                                     shadow_stack_t::reduce)
   shadow_stack_reducer;
+
+typedef shadow_stack_top_bot_t
+    _Hyperobject(shadow_stack_top_bot_t::identity,
+                 shadow_stack_top_bot_t::reduce) shadow_stack_top_bot_reducer;
 
 #endif
